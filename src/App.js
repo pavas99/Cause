0e0d55a413e7c7bca77759555ddeb83f81f0eac4
Pavan <jpavank90@gmail.com>
2020-04-22 00:05:09 -0700

    A Cause Begin

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import motivation from './motivation1.jpg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={motivation} alt="logo" />
+      </header>
+    </div>
+  );
+}
+
+export default App;
