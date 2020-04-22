f33dc9b533040663e5999f20a01aada9be2d1a3d
Pavan <jpavank90@gmail.com>
2020-04-22 00:19:55 -0700

    check ci

--- src/App.js
+++ src/App.js
@@ -6,6 +6,7 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
+        <h1>Good Day!</h1>
         <img src={motivation} alt="logo" />
       </header>
     </div>
